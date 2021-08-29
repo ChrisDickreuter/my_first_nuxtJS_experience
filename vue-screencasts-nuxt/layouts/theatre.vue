a3f2a93db1af6aa0718a8ab05db364ee497695a4
Christian Dickreuter <christian@dickreuter-digital.de>
2021-08-29 15:14:17 +0200

    Added default and alternate layouts

---
+++ vue-screencasts-nuxt/layouts/theatre.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div id="theatre-body">
+            <nuxt />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+    #theatre-body {
+        background-color: black;
+        color: white;
+    }
+</style>
