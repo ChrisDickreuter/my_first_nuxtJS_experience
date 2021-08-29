f944347cc22283f084aa68731b280eea1b7955a4
Christian Dickreuter <christian@dickreuter-digital.de>
2021-08-29 14:49:22 +0200

    Added pages and subpages

---
+++ vue-screencasts-nuxt/pages/videos/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Videos</h1>
+        <div v-for="video in videos" :key="video.id">
+            <nuxt-link :to="`/videos/${video.id}`">{{ video.name }}</nuxt-link>
+        </div>
+        <nuxt-child />
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                videos: [{
+                 id: '16',
+                 name: 'Intro to NuxtJS'   
+                },{
+                 id: '1',
+                 name: 'Intro to VueJS'   
+                },{
+                 id: '71',
+                 name: 'Advanced Techniques for Libary X'   
+                }]
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
